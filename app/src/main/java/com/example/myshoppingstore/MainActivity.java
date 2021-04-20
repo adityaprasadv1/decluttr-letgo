c9a03a3a0a0f47e2307e7fd6ea9afc4c3ca367f5
DugyalaPramod <33366936+DugyalaPramod@users.noreply.github.com>
2021-04-20 14:36:13 -0400

    Splash commit

--- app/src/main/java/com/example/myshoppingstore/MainActivity.java
+++ app/src/main/java/com/example/myshoppingstore/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -12,8 +13,13 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent loginIntent = new Intent(MainActivity.this, Login.class);
-        startActivity(loginIntent);
-        finish();
+        new Handler().postDelayed(new Runnable() { //using predefined postDelayed method to keep activity waiting for 3 seconds before doing any operation
+            @Override
+            public void run() { //moving into home page when 3 seconds delay time is reached
+                Intent splashIntent = new Intent(MainActivity.this, Login.class);
+                startActivity(splashIntent);
+                finish(); //stopping app from going back to the splash screen by closing/finishing this activity
+            }
+        }, 3000); //time set to 3 seconds
     }
 }
